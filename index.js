bd74c27a6327ddd49c6d3031fab34d282c407caf
Duc Dang <mrtrinh5293@gmail.com>
2018-04-26 20:31:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function 
